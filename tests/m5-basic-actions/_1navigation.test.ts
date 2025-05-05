938260cd3722c08839140a70e676c3f166658587
meganlester <trainee.megan.lester@sigmalabs.co.uk>
2025-05-05 12:21:38 +0100

    git ignore venv

---
+++ tests/m5-basic-actions/_1navigation.test.ts
@@ -0,0 +1,11 @@
+import {test, expect} from '@playwright/test';
+
+const homeTitle = 'Credit Association';
+const savingsTitle = 'Save with us';
+
+test('Back, forward, reload (refresh) test', async ({page}) => {
+    await page.goto('/');
+
+    await page.goto('/savings.html');
+    await expect(page).toHaveTitle(savingsTitle)
+})
