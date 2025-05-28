3e160f59dc367d93b3d94b7b281444b7c39dc679
meganlester <trainee.megan.lester@sigmalabs.co.uk>
2025-05-28 21:44:32 +0100

    basic actions

--- tests/m5-basic-actions/_2fill_click.test.ts
+++ tests/m5-basic-actions/_2fill_click.test.ts
@@ -7,3 +7,40 @@ test('filltest', async ({page}) => {
 
     await page.getByLabel('Date of birth').fill('2022-10-10');
 })
+
+test('click demo', async ({page}) => {
+    await page.goto('/');
+
+    const btn = page.getByRole('button', {name: 'Register'});
+    await btn.click({clickCount: 5});
+    await btn.click({button: 'right'})
+})
+
+test('check box', async ({page}) => {
+    await page.goto('/')
+
+    const checkbox = page.getByRole('checkbox');
+    const textarea = page.locator('#textarea');
+    const message = 'mmmmeeddgh'
+    await checkbox.check()
+    await textarea.fill(message)
+
+    await expect(textarea).toHaveValue(message);
+
+})
+
+test('select box', async ({page})=> {
+    page.goto('/savings.html');
+
+    const deposit = page.getByTestId('deposit');
+    const period = page.getByTestId('period');
+    const result = page.getByTestId('result');
+
+    await deposit.fill('500');
+
+    await period.selectOption('2 Years');
+    await expect(result).toContainText('After 2 Years you will earn $60.00 on your deposit');
+
+    await period.selectOption({label: '1 year'});
+    await expect(result).toContainText('After 1 Year you will earn $25.00 on your deposit');
+})
---
+++ tests/m5-basic-actions/_3everything.test.ts
@@ -0,0 +1,44 @@
+import {test, expect} from '@playwright/test'
+
+
+test('check test', async({page}) => {
+    await page.goto('/');
+    await expect(page).toHaveTitle('Credit Association');
+
+    const checkbox = page.getByRole('checkbox');
+    const textarea = page.locator('#textarea');
+    const message = 'ftvughkvkguh'
+
+    await expect(textarea).toBeDisabled();
+    await expect(textarea).toBeEmpty();
+
+    await checkbox.check()
+    await expect(textarea).toBeEnabled();
+
+    await textarea.fill(message);
+    await expect(textarea).toHaveValue(message);
+
+    await page.getByRole('button', {name: 'Register'}).click();
+
+    const feedback = page.locator('.invalid-feedback');
+    await expect(feedback).toHaveCount(3);
+    for (const message of await feedback.all()) {
+        await expect(message).toBeVisible();
+    }
+    await expect(feedback.first()).toContainText('Valid first name is required')
+})
+
+test('test with negating matchers', async({page}) => {
+    await page.goto('/');
+    await expect(page.getByTestId('location')).toContainText('New York');
+
+    await expect(page.getByTestId('location')).not.toContainText('London');
+})
+
+test('test with soft assertions', async({page}) => {
+    await page.goto('/');
+    await expect.soft(page.getByTestId('location')).toContainText('Mumbai');
+    await expect.soft(page.getByTestId('location')).toContainText('Tokyo');
+    await expect(page.getByTestId('location')).toContainText('New York');
+
+})
